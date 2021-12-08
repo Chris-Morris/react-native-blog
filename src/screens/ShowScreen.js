0a2c8efda68e65531d2015b27a165c2c4d51f9b3
Chris Morris <chrismorris87@hotmail.co.uk>
2021-12-08 10:44:23 +0000

    Abstracts form logic into BlogPostForm

--- src/screens/ShowScreen.js
+++ src/screens/ShowScreen.js
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import { Context } from "../context/BlogContext";
 import { EvilIcons } from '@expo/vector-icons';
 
@@ -19,7 +19,7 @@ const ShowScreen = ({ navigation }) => {
 ShowScreen.navigationOptions = ({navigation}) => {
     return {
         headerRight: () => (
-            <TouchableOpacity onPress={() => navigation.navigate('Edit')}>
+            <TouchableOpacity onPress={() => navigation.navigate('Edit', { id: navigation.getParam('id')})}>
                 <EvilIcons name="pencil" size={35} />
             </TouchableOpacity>
         ),
